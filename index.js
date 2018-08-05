4d031eaa6191d69d106826924fb8df64a1650f82
advanced_bear <advanced.bear@gmail.com>
2018-08-05 21:34:42 +0900

    tsselectを使わず、EPGStationのDrop情報を使うように

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const _channel = process.env.CHANNELNAME
 const _title = process.env.NAME
 const _description = process.env.DESCRIPTION
 const _programid = process.env.PROGRAMID
+const _recordedid = process.env.RECORDEDID
 const _date = new Date(Number(process.env.STARTAT)).toLocaleDateString("japanese", {year: 'numeric', month: '2-digit', day: '2-digit', weekday: 'long'})
 const _startAt = new Date(Number(process.env.STARTAT)).toLocaleTimeString("japanese")
 const _endAt = new Date(Number(process.env.ENDAT)).toLocaleTimeString("japanese")
@@ -25,7 +26,6 @@ try {
     process.exit()
 }
 const _host = _config.host // EPGStationの動作するホストアドレス
-const _tsselect = path.join(__dirname, "tsselect") // tsselectの実行ファイルを指定（フォルダ直下に配置）
 const _hostName = !_config.basicId ? "http://"+_host : "http://"+_config.basicId+':'+_config.basicPass+'@'+_host //動作ホストのアドレス（BASIC認証対応）
 const webhookURL = _config.webhookURL.split('/') // DiscordのWebhookアドレス
 const webhook = new Discord.WebhookClient(webhookURL[5],webhookURL[6]) //Discord Webhookの初期化
@@ -37,32 +37,18 @@ var getRecorded = (recordedId, callback)=>{
     })
 }
 var getProgram = (programlId, callback)=>{
-    // チャンネルIDを用いてMirakurun API経由で番組情報を取得する
+    // 番組IDを用いてMirakurun API経由で番組情報を取得する
     request.get(_hostName+":40772/api/programs/"+programlId, (err, res, body)=>{
         !err ? callback(body): callback(err)
     })
 }
-var dropCheck = (fileName, callback)=>{
-    // ファイルパスを与えるとTSファイルのドロップチェックを行う
-    // 映像PIDの情報を連想配列として返す
-    // callback = {total: 12345, d: 0, e: 0, scrambling: 0, offset: 12345}
-    exec(_tsselect,[fileName],{maxBuffer: 4096*1024} , (err, stdout, stderr)=>{
-        if(!err) {
-            let vPIDLine, PIDLine = []
-            let result = iconv.decode(stdout, 'utf8').split(/\r\n|\r|\n/)
-            for(line of result) {
-                if(/pid=0x\d/.test(line)){
-                    PIDLine.push(JSON.parse('{"'+line.replace(/\s+/g,"").replace(/=/g, '":"').replace(/,/g, '", "')+'"}'))
-                } // PIDに関する行を抽出し、空白と記号類を連想配列に用いる記号へ変換
-            }
-            vPIDLine = PIDLine.sort((a, b)=> {
-                if (Number(a.total) > Number(b.total)) return -1
-                if (Number(a.total) < Number(b.total)) return 1
-                return 0
-            }) // total値順にソートを行うことで映像PIDが配列の先頭に来るように
-            callback(vPIDLine[0])
-        } else {
-            callback(null) // tsselect実行時にエラーが発生した場合はnullを返す
+var dropCheck = (callback)=>{
+    // ドロップ情報を取得して返す
+    let recInfo = getRecorded(_recordedid, (json)=>{
+        try{
+            callback(json.errorCnt, json.dropCnt, json.scramblingCnt)
+        } catch(e) {
+            callback(-1, -1 ,-1)
         }
     })
 }
@@ -78,12 +64,12 @@ if(process.argv[2] === 'start'){
 else if(process.argv[2] === 'end'){
     // 録画終了時に投稿するメッセージ
     mes = ":pause_button: "+' __**'+_title+'**__\n```'+_startAt+'～'+_endAt+'［'+_channel+'］\n'
-    dropCheck(_Path, (vPID)=>{
-        if(vPID==null) mes += "!===== Cannot load recorded file! =====!```" // 実行結果がnullの場合
-        else if(vPID.d!='0'){
+    dropCheck((err, drop, scr)=>{
+        if(err==-1) mes += "!===== Cannot load recorded file! =====!```" // 実行結果がnullの場合
+        else if(err!='0'){
             // 映像PIDのd値（ドロップ値）が0でない場合≒ドロップがある場合は詳細を投稿（メンション付き）
             mes += "!===== This MEPG-TS has dropped frame! =====!\n"
-            mes += 'Total:     '+vPID.total+'\nDrop:      '+vPID.d+'\nError:     '+vPID.e+'\nScrmbling: '+vPID.scrambling+'```\@everyone'
+            mes += 'Error:     '+err+'\nDrop:      '+drop+'\nScrmbling: '+scr+'```\@everyone'
         } else {
             // 映像PIDのd値が0の場合はドロップがないのでその旨を投稿
             mes += "!===== This MPEG-TS has no drop =====!```"
